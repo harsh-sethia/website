0dd79431f45929d7c62fe96e0dc332aade84242f
Harsh Sethia <sethia.h@husky.neu.edu>
2020-10-17 23:07:38 +0530

    New thing

---
+++ precache-manifest.48a9836443ed5adbdbe62b22b4a090f6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c7ae70b46c5efefb8256b85282ea47",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "cef384f1df57df9ddf89",
+    "url": "/website/static/css/2.568af6ef.chunk.css"
+  },
+  {
+    "revision": "a455a979bfb4c08daef9",
+    "url": "/website/static/css/main.4d1033dd.chunk.css"
+  },
+  {
+    "revision": "cef384f1df57df9ddf89",
+    "url": "/website/static/js/2.bb109aa8.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/website/static/js/2.bb109aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a455a979bfb4c08daef9",
+    "url": "/website/static/js/main.54aee726.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "78334defb5564633685c3867f6fd1a1f",
+    "url": "/website/static/media/CFN.78334def.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42940d9385f1f86f477273265a01a360",
+    "url": "/website/static/media/my-logo-two.42940d93.png"
+  },
+  {
+    "revision": "a86a2e1c78806613413674b9187840fa",
+    "url": "/website/static/media/my-logo.a86a2e1c.png"
+  },
+  {
+    "revision": "77c286a1af9932202dff0504f8ac8bfb",
+    "url": "/website/static/media/my-photo.77c286a1.jpg"
+  },
+  {
+    "revision": "741e3e15acc954a12ebc4312212c13a7",
+    "url": "/website/static/media/sis.741e3e15.png"
+  }
+]);
